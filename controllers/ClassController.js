17a6cb08704604f9d191f22c1f41de41fef0cc5f
BeibeiYang <chrisyangber@hotmail.com>
2024-08-23 07:05:33 +1000

    get single class controller

--- controllers/ClassController.js
+++ controllers/ClassController.js
@@ -1,4 +1,4 @@
-import { getAllClasses } from "../models/ClassModel.js";
+import { getAllClasses, getClass } from "../models/ClassModel.js";
 
 export const classListAction = async (req, res) => {
   try {
@@ -10,6 +10,19 @@ export const classListAction = async (req, res) => {
     });
   } catch (error) {
     // res.status(500).json({ error: "Failed to fetch classes" });
+    console.log(error);
+  }
+};
+
+export const classShowAction = async (req, res) => {
+  try {
+    const [course] = await getClass(+req.params.classId);
+    console.log(course);
+    res.status(200).render("class", {
+      title: course[0].className,
+      course,
+    });
+  } catch (error) {
     console.log(error);
   }
 };
--- routes/classRoutes.js
+++ routes/classRoutes.js
@@ -1,6 +1,11 @@
 import { Router } from "express";
-import { classListAction } from "../controllers/ClassController.js";
+import {
+  classListAction,
+  classShowAction,
+} from "../controllers/ClassController.js";
 
 export const classRouter = Router();
 
 classRouter.get("/", classListAction);
+
+classRouter.get("/:classId", classShowAction);
