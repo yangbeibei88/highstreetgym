836ea8dad67ac8425005a3a836fc4382ad9056d2
BeibeiYang <chrisyangber@hotmail.com>
2024-08-22 21:45:36 +1000

    classes getAll method

--- controllers/ClassController.js
+++ controllers/ClassController.js
@@ -4,8 +4,12 @@ export const classListAction = async (req, res) => {
   try {
     const [classes] = await getAllClasses();
     console.log(classes);
-    res.status(200).json({ classes });
+    res.status(200).render("classes", {
+      title: "All classes",
+      classes,
+    });
   } catch (error) {
-    res.status(500).json({ error: "Failed to fetch classes" });
+    // res.status(500).json({ error: "Failed to fetch classes" });
+    console.log(error);
   }
 };
--- models/ClassModel.js
+++ models/ClassModel.js
@@ -1,9 +1,9 @@
 import { pool } from "../config/db.js";
 
-const getPool = await pool();
+const dbPool = await pool();
 
 export const getAllClasses = async () => {
-  const conn = await getPool.getConnection();
+  const conn = await dbPool.getConnection();
   try {
     const sql = "SELECT * FROM `classes`";
     return await conn.execute(sql);
