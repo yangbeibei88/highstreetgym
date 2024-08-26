2250ffa45ba8d947dd709a03bb06190b87ac41ab
BeibeiYang <chrisyangber@hotmail.com>
2024-08-26 18:44:42 +1000

    refactor validation with express-validator

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,92 +1,32 @@
-import { findUserByEmail } from "../models/UserModel.js";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import asyncHandler from "express-async-handler";
+// eslint-disable-next-line import/no-extraneous-dependencies
 import {
-  validateText,
-  validatePhoneNumber,
+  compareString,
   validateEmail,
   validatePassword,
-  compareString,
+  validatePhoneNumber,
+  validateText,
 } from "../utils/validation.js";
+import { findUserByEmail } from "../models/UserModel.js";
 
-export const renderSignupAction = async (req, res, next) => {
+export const renderSignupAction = (req, res, next) => {
   res.status(200).render("signup", { title: "Sign Up" });
 };
-export const renderLoginAction = async (req, res, next) => {
+
+export const renderLoginAction = (req, res, next) => {
   res.status(200).render("login", { title: "Login" });
 };
-export const authenticateSignupAction = async (req, res, next) => {
-  console.log(req.body);
 
-  // 1) SCOPE FIELDS
-  const inputData = {
-    register_firstname: req.body.register_firstname,
-    register_lastname: req.body.register_lastname,
-    register_mobilenumber: req.body.register_mobilenumber,
-    register_email: req.body.register_email,
-    register_password: req.body.register_password,
-    register_confirmPassword: req.body.register_confirmPassword,
-  };
-
-  // 2) FORM FIELDS VALIDATION
-  let errors = [
-    {
-      register_firstname: validateText(
-        "firstname",
-        req.body.register_firstname,
-        2,
-        20,
-      ),
-    },
-    {
-      register_lastname: validateText(
-        "lastname",
-        req.body.register_lastname,
-        2,
-        20,
-      ),
-    },
-    {
-      register_mobilenumber: validatePhoneNumber(
-        "phone number",
-        req.body.register_mobilenumber,
-      ),
-    },
-    {
-      register_email: validateEmail("Email", req.body.register_email),
-    },
-    {
-      register_password: validatePassword(
-        "password",
-        req.body.register_password,
-      ),
-    },
-    {
-      register_confirmPassword: compareString(
-        "passwords",
-        req.body.register_password,
-        req.body.register_confirmPassword,
-      ),
-    },
-  ];
-
-  // 3) VALIDATE IF EMAIL ALREADY EXITS IN DB
-  const [existingUser] = await findUserByEmail(req.body.register_email);
-  if (existingUser) {
-    errors.register_password =
-      "This email already exists, please try to login.";
-  }
-
-  // 4) FILTER ERROR VALUES THAT ARE NOT NULL
-  errors = errors.filter((obj) => Object.values(obj)[0] !== null);
-
-  if (errors.length > 0) {
-    res.status(400).render("signup", {
-      errors,
-      inputData,
-    });
-  }
-
-  // 4) DATA SANITISATION
-};
+export const authenticateSignupAction = asyncHandler(async (req, res, next) => {
+  // 1) VALIDATE & SANITISE FIELDS
+  validateText("register_firstname", 2, 20, true);
+  validateText("register_surname", 2, 20, true);
+  validatePhoneNumber("register_mobilenumber", true);
+  validateEmail("register_email", true, findUserByEmail, true);
+  validatePassword("register_password", true);
+  compareString("Passwords", "register_password", "register_confirmPassword");
+});
 
 export const authenticateLoginAction = async (req, res, next) => {
   console.log(req.body);
--- utils/validation.js
+++ utils/validation.js
@@ -1,84 +1,111 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { body } from "express-validator";
+
 // validate a string
-export const validateText = (
-  name,
-  value,
-  min = 0,
-  max = 254,
-  required = true,
-) => {
-  value = value.trim();
-  if (required && value.length === 0) {
-    return `${name} is required.`;
+export const validateText = (name, min = 0, max = 254, required = true) => {
+  let chain = body(name).trim();
+  if (required) {
+    chain = chain.notEmpty().withMessage(`${name} is required.`);
   }
-  if (value.length > 0 && value.length < min) {
-    return `${name} is too short, should be more than ${min} characters.`;
+  if (min > 0) {
+    chain = chain
+      .isLength({ min })
+      .withMessage(
+        `${name} is too short, should be more than ${min} characters.`,
+      );
   }
-  if (value.length > max) {
-    return `${name} is too long, should not exceed ${max} characters.`;
+  if (max > 0) {
+    chain = chain
+      .isLength({ max })
+      .withMessage(`${name} is too long, should not exceed ${max} characters.`);
   }
-  return null;
+  chain = chain.escape();
+
+  return chain;
 };
 
 // validate an email address
 // https://stackoverflow.com/questions/46155/how-can-i-validate-an-email-address-in-javascript
-export const validateEmail = (name, value, required = true) => {
-  value = value.trim().toLowerCase();
-  if (required && value.length === 0) {
-    return `${name} is required.`;
+export const validateEmail = (
+  name,
+  runInUse = false,
+  cb = undefined,
+  required = true,
+) => {
+  let chain = body(name).trim().toLowerCase();
+
+  if (required) {
+    chain = chain.notEmpty().withMessage(`${name} is required.`);
   }
-  if (value.length > 0) {
-    const re =
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (!re.test(value)) {
-      return `Invalid email address.`;
-    }
-    if (value.length > 254) {
-      return `${name} must not exceed 254 characters.`;
-    }
+
+  chain = chain.isEmail().withMessage(`Invalid email address.`);
+  chain = chain
+    .isLength({ max: 254 })
+    .withMessage(`${name} must not exceed 254 characters.`);
+
+  if (runInUse === true && typeof cb === "function") {
+    chain = chain.custom(async (value) => {
+      const [rows] = await cb(value);
+      if (rows.length > 0) {
+        throw new Error("Email already in use.");
+      }
+    });
   }
-  return null;
+
+  // DON'T ESCAPE EMAIL, BECAUSE A VALIDATE EMAIL ALREADY ESCAPED UNALLOWED CHARACTERS BASED ON RFC 5322
+  // chain = chain.escape();
+  return chain;
 };
 
 // validate phone number
 // https://en.wikipedia.org/wiki/Telephone_numbers_in_Australia#:~:text=Australia%20or%20internationally.-,Mobile%20phones,XXX%20for%20an%20international%20audience.
-export const validatePhoneNumber = (name, value, required = true) => {
-  value = value.trim();
-  if (required && value.length === 0) {
-    return `${name} is required.`;
+export const validatePhoneNumber = (name, required = true) => {
+  let chain = body(name).trim();
+
+  if (required) {
+    chain = chain.notEmpty().withMessage(`${name} is required.`);
   }
-  if (value.length > 0) {
-    const re = /^0[2-478]\d{8}$/;
-    if (!re.test(value)) {
-      return `Invalid phone number. Your phone number must be 10 digits without any spaces, parentheses, or non-numeric characters`;
-    }
-  }
-  return null;
+
+  chain = chain
+    .matches(/^0[2-478]\d{8}$/)
+    .withMessage(
+      "Invalid phone number. Your phone number must be 10 digits without any spaces, parentheses, or non-numeric characters",
+    );
+
+  chain = chain.escape();
+
+  return chain;
 };
 
 // Validate password
-export const validatePassword = (name, value, required = true) => {
-  value = value.trim();
-  if (required && value.length === 0) {
-    return `${name} is required.`;
-  }
-  const re =
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-  if (!re.test(value)) {
-    return `Invalid password. Your password must be at least 8 characters long, containing uppercase(s), lowercase(s), number(s) and special chars.`;
+export const validatePassword = (name, required = true) => {
+  let chain = body(name).trim();
+  if (required) {
+    chain = chain.notEmpty().withMessage(`${name} is required.`);
   }
 
-  return null;
+  chain = chain
+    .isLength({ max: 254 })
+    .withMessage(`${name} must not exceed 254 characters.`);
+
+  chain = chain
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+    )
+    .withMessage(
+      "Invalid password. Your password must be at least 8 characters long, containing uppercase(s), lowercase(s), number(s) and special chars.",
+    );
+
+  return chain;
 };
 
 // compare password
-export const compareString = (name, str1, str2) => {
-  str1 = str1.trim();
-  str2 = str2.trim();
+export const compareString = (plural, str1Name, str2Name) => {
+  const chain1 = body(str1Name).trim();
 
-  if (str1.length > 0 || str2.length > 0) {
-    if (str1 !== str2) {
-      return `${name} do not match.`;
-    }
-    return null;
-  }
+  let chain2 = body(str2Name).trim();
+
+  chain2 = chain2.equals(chain1).withMessage(`${plural} do not match.`);
+
+  return chain2;
 };
