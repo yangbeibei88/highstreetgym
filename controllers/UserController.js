cf048e168382b279c27db876e3a1bca43457991a
BeibeiYang <chrisyangber@hotmail.com>
2024-08-28 12:50:52 +1000

    added my account pages title

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,18 +1,18 @@
 export const renderMydashboardAction = async (req, res, next) =>
-  res.status(200).render("my-dashboard");
+  res.status(200).render("my-dashboard", { title: "My Dashboard" });
 
 export const renderMybookingsAction = async (req, res, next) =>
-  res.status(200).render("my-bookings");
+  res.status(200).render("my-bookings", { title: "My Bookings" });
 
 export const renderMyarticlesAction = async (req, res, next) => {
-  res.status(200).render("my-articles");
+  res.status(200).render("my-articles", { title: "My Articles" });
 };
 export const renderMycommentsAction = async (req, res, next) => {
-  res.status(200).render("my-comments");
+  res.status(200).render("my-comments", { title: "My Comments" });
 };
 export const renderMyprofileAction = async (req, res, next) => {
-  res.status(200).render("my-profile");
+  res.status(200).render("my-profile", { title: "My Profile" });
 };
 export const renderChangePasswordAction = async (req, res, next) => {
-  res.status(200).render("change-password");
+  res.status(200).render("change-password", { title: "Change Password" });
 };
