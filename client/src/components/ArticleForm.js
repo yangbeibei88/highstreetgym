589f6c2f0636115c4b6012a7ca5ec081fca7178d
BeibeiYang <chrisyangber@hotmail.com>
2024-08-31 17:25:57 +1000

    fix articleForm elements, instead using id

--- client/src/components/ArticleForm.js
+++ client/src/components/ArticleForm.js
@@ -3,14 +3,12 @@ import { QuillEditor } from "./QuillEditor.js";
 export class ArticleForm {
   constructor() {
     this._articleEditorEl = document.getElementById("articleEditor");
-    this._hiddenContentInput = document.getElementById(
-      "form-create-article",
-    ).elements.articleContent;
+    this._hiddenContentInput = document.getElementById("articleContent");
     this.editor();
   }
 
   editor() {
-    if (this._articleEditorEl) {
+    if (this._articleEditorEl && this._hiddenContentInput) {
       this.articleEditor = new QuillEditor(
         this._articleEditorEl,
         this._hiddenContentInput,
