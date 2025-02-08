3dbff45c9d6fe1efcff8c33c3318e259e12bb653
BeibeiYang <chrisyangber@hotmail.com>
2025-02-09 01:01:31 +1000

    create and export on pool instance

--- config/db.js
+++ config/db.js
@@ -1,16 +1,26 @@
 import mysql from "mysql2/promise";
 
-export const pool = async () => {
-  try {
-    return mysql.createPool({
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      database: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-    });
-  } catch (error) {
-    console.log(error);
-    // throw error;
-  }
-};
+// export const pool = async () => {
+//   try {
+//     return mysql.createPool({
+//       host: process.env.DB_HOST,
+//       port: process.env.DB_PORT,
+//       database: process.env.DB_NAME,
+//       user: process.env.DB_USER,
+//       password: process.env.DB_PASSWORD,
+//     });
+//   } catch (error) {
+//     console.log(error);
+//     // throw error;
+//   }
+// };
+
+// instead of exporting a function that creates a new pool every time, create the pool once when the application starts
+
+export const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+});
